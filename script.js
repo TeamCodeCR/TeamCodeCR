86f39fcdb796f5b820eb08fc6781ebe697e5f19d
TeamCodeCR <100386430+TeamCodeCR@users.noreply.github.com>
2022-02-24 17:25:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Páginas web", "Aplicaciones web", "Lo que necesitas"],
+        strings: ["Gana más clientes", "Obtén control de tu negocio", "Actualízate ;)", "Justo lo que necesitas"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
